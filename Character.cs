de6cee6226bf3eb470ceb2aa611fae96a3c62d6d
Aaleeyah Ivy <53322518+AaleeyahIvy@users.noreply.github.com>
2020-11-30 18:29:58 -0500

    Create Character.cs

---
+++ Character.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Character
+{
+	public string name;
+	public int health;
+	public static Random rnd = new Random();
+	int attack = rnd.Next(1,35);
+	
+	public Character(string name, int health)
+	{
+		this.name = name;
+		this.health = health;
+	}
+	
